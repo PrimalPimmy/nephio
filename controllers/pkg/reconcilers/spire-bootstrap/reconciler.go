2b5f48afdf62ec75356183951a6ca0c0c529cd85
PrimalPimmy <Prashant20.pm@gmail.com>
2024-07-01 14:46:21 +0530

    Some testing
    Signed-off-by: PrimalPimmy <Prashant20.pm@gmail.com>

--- controllers/pkg/reconcilers/spire-bootstrap/reconciler.go
+++ controllers/pkg/reconcilers/spire-bootstrap/reconciler.go
@@ -18,16 +18,10 @@ package bootstrapsecret
 
 import (
 	"context"
-	"fmt"
-	"io"
-	"net/http"
-	"os"
-	"time"
+	"flag"
+	Logg "log"
 
 	reconcilerinterface "github.com/nephio-project/nephio/controllers/pkg/reconcilers/reconciler-interface"
-	"github.com/spiffe/go-spiffe/v2/spiffeid"
-	"github.com/spiffe/go-spiffe/v2/spiffetls/tlsconfig"
-	"github.com/spiffe/go-spiffe/v2/svid/jwtsvid"
 	"github.com/spiffe/go-spiffe/v2/workloadapi"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
@@ -169,7 +163,18 @@ func (r *reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Resu
 
 	// // Example: Update the status if necessary
 
-	err = run(ctx)
+	workloadAPIAddr := flag.String("workload-api-addr", "", "Workload API Address")
+	flag.Parse()
+
+	var opts []workloadapi.ClientOption
+	if *workloadAPIAddr != "" {
+		opts = append(opts, workloadapi.WithAddr(*workloadAPIAddr))
+	}
+
+	Logg.Println("Watching...")
+	err = workloadapi.WatchX509Context(context.Background(), watcher{}, opts...)
+	Logg.Fatal("Error: ", err)
+
 	if err != nil {
 		log.Error(err, "Spire auth didnt work")
 	}
@@ -177,77 +182,20 @@ func (r *reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Resu
 	return reconcile.Result{}, nil
 }
 
-const (
-	serverURL  = "spire-server:8081"
-	socketPath = "unix:///spiffe-workload-api/agent.sock"
-)
+type watcher struct{}
 
-func run(ctx context.Context) error {
-	// Time out the example after 30 seconds. This prevents the example from hanging if the workloads are not properly registered with SPIRE.
-	ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
-	defer cancel()
-
-	// Create client options to setup expected socket path,
-	// as default sources will use value from environment variable `SPIFFE_ENDPOINT_SOCKET`
-	clientOptions := workloadapi.WithClientOptions(workloadapi.WithAddr(socketPath))
-
-	// Create X509 source to fetch bundle certificate used to verify presented certificate from server
-	x509Source, err := workloadapi.NewX509Source(ctx, clientOptions)
-	if err != nil {
-		return fmt.Errorf("unable to create X509Source: %w", err)
+func (watcher) OnX509ContextUpdate(x509Context *workloadapi.X509Context) {
+	Logg.Println("Update:")
+	Logg.Println("  SVIDs:")
+	for _, svid := range x509Context.SVIDs {
+		Logg.Printf("    %s\n", svid.ID)
 	}
-	defer x509Source.Close()
-
-	// Create a `tls.Config` with configuration to allow TLS communication, and verify that presented certificate from server has SPIFFE ID `spiffe://example.org/server`
-	serverID := spiffeid.RequireFromString("spiffe://example.org/server")
-	tlsConfig := tlsconfig.TLSClientConfig(x509Source, tlsconfig.AuthorizeID(serverID))
-
-	client := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: tlsConfig,
-		},
+	Logg.Println("  Bundles:")
+	for _, bundle := range x509Context.Bundles.Bundles() {
+		Logg.Printf("    %s (%d authorities)\n", bundle.TrustDomain(), len(bundle.X509Authorities()))
 	}
+}
 
-	req, err := http.NewRequest("GET", serverURL, nil)
-	if err != nil {
-		return fmt.Errorf("unable to create request: %w", err)
-	}
-
-	// As default example is using server's ID,
-	// It doesn't have to be an SPIFFE ID as long it follows JWT SVIDs the guidelines (https://github.com/spiffe/spiffe/blob/main/standards/JWT-SVID.md#32-audience)
-	audience := serverID.String()
-	args := os.Args
-	if len(args) >= 2 {
-		audience = args[1]
-	}
-
-	// Create a JWTSource to fetch SVIDs
-	jwtSource, err := workloadapi.NewJWTSource(ctx, clientOptions)
-	if err != nil {
-		return fmt.Errorf("unable to create JWTSource: %w", err)
-	}
-	defer jwtSource.Close()
-
-	// Fetch JWT SVID and add it to `Authorization` header,
-	// It is possible to fetch JWT SVID using `workloadapi.FetchJWTSVID`
-	svid, err := jwtSource.FetchJWTSVID(ctx, jwtsvid.Params{
-		Audience: audience,
-	})
-	if err != nil {
-		return fmt.Errorf("unable to fetch SVID: %w", err)
-	}
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", svid.Marshal()))
-
-	res, err := client.Do(req)
-	if err != nil {
-		return fmt.Errorf("unable to issue request to %q: %w", serverURL, err)
-	}
-	defer res.Body.Close()
-
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		return fmt.Errorf("error reading response body: %w", err)
-	}
-	log.Log.Info("%s", body)
-	return nil
+func (watcher) OnX509ContextWatchError(err error) {
+	Logg.Println("Error:", err)
 }
